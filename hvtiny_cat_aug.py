3dfb010ef85d6e91b5667ae59dab004b63b6ca4e
radu-dogaru <radu_d@ieee.org>
2026-04-10 21:44:46 +0300

    Restore original comments and structure in hvtiny.py

--- hvtiny_cat_aug.py
+++ hvtiny_cat_aug.py
@@ -22,8 +22,8 @@ def hvtiny(input_shape, num_classes, profil=[1, 3, 2], k=60, nl=(1,1), hid=True,
         else:
             inputs = keras.Input(shape=input_shape)
         n = int(k)
-	#
-	x=data_augmentation(inputs)
+		# insert augmenter 
+		x=data_augmentation(inputs)
         # convolutional macro-block 0
         x = keras.layers.Conv2D(n, kernel_size, padding=pad)(x)  # Separable
         for i in range(1, nl[0]+1) :
